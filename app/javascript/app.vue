b9fd95de3d1c2afabe545388e5ff7e082206656d
Matt Lewis <matt@mplewis.com>
2017-10-09 09:16:11 -0600

    app: render basic vue stuff with a single controller

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,14 +1,20 @@
 <template>
   <div id="app">
     <p>{{ message }}</p>
+    <child name="wonderful Rails user" />
   </div>
 </template>
 
 <script>
+import Child from './child'
+
 export default {
+  components: {
+    child: Child
+  },
   data: function () {
     return {
-      message: "Hello Vue!"
+      message: "Hello! This is Vue. My child component is here:"
     }
   }
 }
---
+++ app/javascript/child.vue
@@ -0,0 +1,23 @@
+<template>
+  <p><em>{{ message }}</em></p>
+</template>
+
+<script>
+export default {
+  props: {
+    name: { type: String, required: true },
+  },
+  data: function() {
+    return {
+      message: `Hello, ${this.name}! I'm the child component.`
+    }
+  }
+};
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
